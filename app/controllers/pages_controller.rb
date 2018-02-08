d123daadff01f9940ecdac816873b2c7c6564d25
Michael Wilson <info@michaelwilsion.io>
2018-02-08 14:10:59 -0500

    Completed section of course to edit pages and update them in the database using a form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,9 +12,23 @@ class PagesController < ApplicationController
     end
 
     def create
-        page_params = params.require(:page).permit(:title, :body, :slug)
         @page = Page.new(page_params)
         @page.save
         redirect_to @page
     end
+
+    def edit
+        @page = Page.find(params[:id])
+    end
+
+    def update
+        @page = Page.find(params[:id])
+        @page.update(page_params)
+        redirect_to @page
+    end
+
+    private
+        def page_params
+            params.require(:page ).permit(:title, :body, :slug)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   post '/pages', to: 'pages#create'
   get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
+  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  patch 'pages/:id', to: 'pages#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
