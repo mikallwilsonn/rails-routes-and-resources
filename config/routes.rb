d9623bceb8f5429e477910229ea35ca513c7e896
Michael Wilson <info@michaelwilsion.io>
2018-02-08 15:26:36 -0500

    Completed the functionality to delete pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/pages', to: 'pages#index' 
-  post '/pages', to: 'pages#create'
-  get '/pages/new', to: 'pages#new', as: 'new_page'
-  get '/pages/:id', to: 'pages#show', as: 'page'
-  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
-  patch 'pages/:id', to: 'pages#update'
+  resources :pages 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
