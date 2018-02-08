37972d124d8b27a12f18676efe7133a0a966a025
Michael Wilson <info@michaelwilsion.io>
2018-02-08 12:08:37 -0500

    Addded a route for creating new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   get '/pages', to: 'pages#index' 
+  get '/pages/new', to: 'pages#new'
   get '/pages/:id', to: 'pages#show', as: 'page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
